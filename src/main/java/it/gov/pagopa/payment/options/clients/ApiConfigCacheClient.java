a1e5fe412deca5e4284082f3213f83f9f5f04536
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-09-11 10:14:53 +0200

    [PPANTT-82] feat: Updated ApiConfigCacheClient and application.properties

--- src/main/java/it/gov/pagopa/payment/options/clients/ApiConfigCacheClient.java
+++ src/main/java/it/gov/pagopa/payment/options/clients/ApiConfigCacheClient.java
@@ -4,6 +4,7 @@ import it.gov.pagopa.payment.options.clients.model.ConfigDataV1;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
+import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import java.util.List;
 
@@ -20,6 +21,7 @@ public interface ApiConfigCacheClient {
    * @return required cache data
    */
   @GET
+  @ClientHeaderParam(name = "Ocp-Apim-Subscription-Key", value = "${ApiConfigCacheClient.ocpSubKey}")
   ConfigDataV1 getCache(@QueryParam("keys") List<String> keys);
 
 }
