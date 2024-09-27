41b58155a56215916d77f2a6a7b99c5840c1547f
giomella <giomella@emeal.nttdata.com>
2024-09-27 16:46:46 +0200

    improved openapi withe app error code info

--- src/test/java/it/gov/pagopa/payment/options/OpenApiGenerationTest.java
+++ src/test/java/it/gov/pagopa/payment/options/OpenApiGenerationTest.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.payment.options;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.test.junit.QuarkusTest;
+import it.gov.pagopa.payment.options.models.enums.AppErrorCodeEnum;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Files;
@@ -14,7 +15,6 @@ import static io.restassured.RestAssured.given;
 class OpenApiGenerationTest {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
-
 
     @Test
     void swaggerSpringPlugin() throws Exception {
@@ -30,11 +30,26 @@ class OpenApiGenerationTest {
 
         Object swagger = objectMapper.readValue(responseString, Object.class);
         String formatted = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(swagger);
+        formatted = formatted.replace("placeholder-for-replace", getAppErrorCodes());
         Path basePath = Paths.get("openapi/");
         Files.createDirectories(basePath);
         Files.write(basePath.resolve("openapi.json"), formatted.getBytes());
-
     }
 
+    private String getAppErrorCodes() {
+        StringBuilder stringBuilder = new StringBuilder();
 
+        stringBuilder.append("PagoPa Payment Options service \\n ### APP ERROR CODES ### \\n\\n\\n <details><summary>Details</summary>\\n **NAME** | **HTTP STATUS CODE** | **DESCRIPTION** \\n- | - | - ");
+        for (AppErrorCodeEnum errorCode : AppErrorCodeEnum.values()) {
+            stringBuilder
+                    .append("\\n **")
+                    .append(errorCode.getErrorCode())
+                    .append("** | *")
+                    .append(errorCode.getStatus())
+                    .append("* | ")
+                    .append(errorCode.getErrorMessage());
+        }
+        stringBuilder.append(" \\n\\n </details> \\n");
+        return stringBuilder.toString();
+    }
 }
