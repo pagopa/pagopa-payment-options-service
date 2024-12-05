28aba5161dc052d169dffd7f39e71ba876ffccb2
svariant <samuele.varianti@nttdata.com>
2024-12-05 14:21:54 +0100

    test: Log key

--- performance-test/src/modules/payment_options_client.js
+++ performance-test/src/modules/payment_options_client.js
@@ -1,8 +1,10 @@
 import http from 'k6/http';
+import {b64encode} from 'k6/encoding';
 const subKey = `${__ENV.OCP_APIM_SUBSCRIPTION_KEY}`;
 
 export function getToService(endpoint, params) {
   let url = endpoint;
+  console.log("TEST", b64encode(subKey));
   let headers = {
     'Ocp-Apim-Subscription-Key': subKey,
     "Content-Type": "application/json"
