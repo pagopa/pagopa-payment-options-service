268b3dfc1085c5163e9f18b88faf5bf6e41a6e63
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2026-01-27 16:36:47 +0100

    fix: [PIDM-1438] fix error duplicate key

--- integration-test/src/step_definitions/support/client/payment_options_client.js
+++ integration-test/src/step_definitions/support/client/payment_options_client.js
@@ -1,4 +1,4 @@
-const { get, post } = require("../utility/axios_common");
+const { get, post, del } = require("../utility/axios_common");
 const fs = require('fs/promises');
 const path = require('path');
 
@@ -6,6 +6,7 @@ const PAY_OPT_HOST = process.env.PAY_OPT_HOST;
 const GPD_V3_HOST = process.env.GPD_V3_HOST;
 const API_TIMEOUT = process.env.API_TIMEOUT;
 const GPD_SUBKEY = process.env.GPD_SUBKEY;
+const SUBKEY = process.env.SUBKEY;
 
 async function loadDebtPosition(filename, idOrg) {
     const basePath = path.join(process.cwd(), 'config', 'gpd-data');
@@ -13,6 +14,11 @@ async function loadDebtPosition(filename, idOrg) {
     let fileContent = await fs.readFile(fullPath, 'utf8');
     fileContent = fileContent.replaceAll("XXXXXXXXXXX", idOrg);
     let payload = JSON.parse(fileContent);
+
+    let iupdToDelete = payload.iupd;
+    if (iupdToDelete) {
+        await deleteDebtPosition(idOrg, iupdToDelete);
+    }
 
     const tomorrow = new Date();
     tomorrow.setDate(tomorrow.getDate() + 1);
@@ -33,11 +39,19 @@ async function loadDebtPosition(filename, idOrg) {
         });
     }
     const resp = await createDebtPosition(idOrg, payload);
+    return (resp.status === 201 || resp.status === 409);
+}
 
-    if (resp.status !== 201 && resp.status !== 409) {
-        console.error(`Failed to load debt position ${filename}. Status: ${resp.status}, Data: ${JSON.stringify(resp.data)}`);
-    }
-    return (resp.status === 201 || resp.status === 409)
+async function deleteDebtPosition(org, iupd) {
+    const url = GPD_V3_HOST + `/organizations/${org}/debtpositions/${iupd}`;
+
+    return await del(url, {
+            timeout: API_TIMEOUT,
+            headers: {
+                "Ocp-Apim-Subscription-Key": GPD_SUBKEY,
+                "Content-Type": "application/json"
+            }
+        });
 }
 
 async function createDebtPosition(org, jsonBody) {
@@ -53,8 +67,6 @@ async function createDebtPosition(org, jsonBody) {
         });
 }
 
-
-
 async function getPaymentOptions(taxCode, noticeNumber, idPsp) {
 
     let params = {};
@@ -69,7 +81,7 @@ async function getPaymentOptions(taxCode, noticeNumber, idPsp) {
         timeout: API_TIMEOUT,
         params,
         headers: {
-            "Ocp-Apim-Subscription-Key": process.env.SUBKEY,
+            "Ocp-Apim-Subscription-Key": SUBKEY,
             "Content-Type": "application/json"
         }
     });
