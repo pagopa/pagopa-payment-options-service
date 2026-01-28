5afe3b72fe68fdb9e550fe771c3cb8c82d4ec469
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2026-01-28 16:18:00 +0100

    fix: [PIDM-912] fix formatting

--- infra/99_locals.tf
+++ infra/99_locals.tf
@@ -2,8 +2,8 @@ locals {
   product = "${var.prefix}-${var.env_short}"
 
   apim = {
-    name                   = "${local.product}-apim"
-    rg                     = "${local.product}-api-rg"
+    name                        = "${local.product}-apim"
+    rg                          = "${local.product}-api-rg"
     payments_options_product_id = "pagopa_payment_options"
     nodo_auth_product_id        = "nodo-auth"
   }
