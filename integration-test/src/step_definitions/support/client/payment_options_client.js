c428250a31cf25a27dde6f5d7ea3d4ddbdaac791
RiccardoGiuliani <144138935+RiccardoGiuliani@users.noreply.github.com>
2026-01-28 11:40:51 +0100

    fix: [PIDM-1438] Fix failing integration tests (#50)
    * feat: [PIDM-912] get payment option under node-auth
    
    * feat: [PIDM-912] get payment option under node-auth
    
    * feat: [PIDM-912] fix integration test
    
    * feat: [PIDM-912] fix integration test
    
    * feat: [PIDM-912] fix integration test
    
    * fix: [PIDM-1438] fix integration tests
    
    * fix: [PIDM-1438] add logs
    
    * fix: [PIDM-1438] fix subkey
    
    * fix: [PIDM-1438] fix subkey
    
    * fix: [PIDM-1438] test fix
    
    * fix: [PIDM-1438] test debug
    
    * fix: [PIDM-1438] fix run docker
    
    * smoke on self-hosted job
    
    * test
    
    * test
    
    * test
    
    * fix: [PIDM-1438] fix run docker
    
    * fix: [PIDM-1438] test
    
    * fix: [PIDM-1438] test
    
    * fix: [PIDM-1438] test
    
    * fix: [PIDM-1438] test
    
    * fix: [PIDM-1438] restored run_docker and code_review
    
    * fix: [PIDM-1438] fix error duplicate key
    
    ---------
    
    Co-authored-by: Francesco <cesareo.francesco@gmail.com>

--- integration-test/src/step_definitions/support/client/payment_options_client.js
+++ integration-test/src/step_definitions/support/client/payment_options_client.js
@@ -1,4 +1,4 @@
-const {get, post} = require("../utility/axios_common");
+const { get, post, del } = require("../utility/axios_common");
 const fs = require('fs/promises');
 const path = require('path');
 
@@ -6,18 +6,56 @@ const PAY_OPT_HOST = process.env.PAY_OPT_HOST;
 const GPD_V3_HOST = process.env.GPD_V3_HOST;
 const API_TIMEOUT = process.env.API_TIMEOUT;
 const GPD_SUBKEY = process.env.GPD_SUBKEY;
+const SUBKEY = process.env.SUBKEY;
 
 async function loadDebtPosition(filename, idOrg) {
     const basePath = path.join(process.cwd(), 'config', 'gpd-data');
     const fullPath = path.join(basePath, filename + '.json');
-    fileContent = await fs.readFile(fullPath, 'utf8');
-    fileContent = fileContent.replaceAll("XXXXXXXXXXX", idOrg)
-    resp = await createDebtPosition(idOrg, JSON.parse(fileContent));
-    return (resp.status === 201 || resp.status === 409)
+    let fileContent = await fs.readFile(fullPath, 'utf8');
+    fileContent = fileContent.replaceAll("XXXXXXXXXXX", idOrg);
+    let payload = JSON.parse(fileContent);
+
+    let iupdToDelete = payload.iupd;
+    if (iupdToDelete) {
+        await deleteDebtPosition(idOrg, iupdToDelete);
+    }
+
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const tomorrowISO = tomorrow.toISOString();
+
+    if (payload.paymentOption) {
+        payload.paymentOption.forEach(option => {
+            if (option.installments) {
+                option.installments.forEach(installment => {
+                    installment.dueDate = tomorrowISO;
+                });
+            }
+            if (option.retentionDate) {
+                const futureRetention = new Date();
+                futureRetention.setDate(futureRetention.getDate() + 2);
+                option.retentionDate = futureRetention.toISOString();
+            }
+        });
+    }
+    const resp = await createDebtPosition(idOrg, payload);
+    return (resp.status === 201 || resp.status === 409);
+}
+
+async function deleteDebtPosition(org, iupd) {
+    const url = GPD_V3_HOST + `/organizations/${org}/debtpositions/${iupd}`;
+
+    return await del(url, {
+            timeout: API_TIMEOUT,
+            headers: {
+                "Ocp-Apim-Subscription-Key": GPD_SUBKEY,
+                "Content-Type": "application/json"
+            }
+        });
 }
 
 async function createDebtPosition(org, jsonBody) {
-    const data = await post(GPD_V3_HOST +
+    return await post(GPD_V3_HOST +
         `/organizations/` + org + `/debtpositions?toPublish=true`,
         jsonBody,
         {
@@ -26,12 +64,8 @@ async function createDebtPosition(org, jsonBody) {
                 "Ocp-Apim-Subscription-Key": GPD_SUBKEY,
                 "Content-Type": "application/json"
             }
-    });
-
-    return data;
+        });
 }
-
-
 
 async function getPaymentOptions(taxCode, noticeNumber, idPsp) {
 
@@ -42,17 +76,15 @@ async function getPaymentOptions(taxCode, noticeNumber, idPsp) {
         params.idBrokerPsp = idPsp;
     }
 
-    const data = await get(PAY_OPT_HOST +
-     `/payment-options/organizations/`+taxCode+`/notices/`+noticeNumber, {
+    return await get(PAY_OPT_HOST +
+        `/payment-options/organizations/` + taxCode + `/notices/` + noticeNumber, {
         timeout: API_TIMEOUT,
         params,
         headers: {
-            "Ocp-Apim-Subscription-Key": process.env.SUBKEY,
+            "Ocp-Apim-Subscription-Key": SUBKEY,
             "Content-Type": "application/json"
         }
     });
-
-    return data;
 }
 
 module.exports = {
